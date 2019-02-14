6aa5d10811a7137af4c14f25431b65d549996b0e
jluna12 <jorge.luna.1@asu.edu>
2019-02-14 11:23:21 -0700

    added login page

---
+++ link.js
@@ -0,0 +1,14 @@
+
+window.onload = function()
+{
+    //window.alert("The page just finished loading.");
+    document.getElementById("button").onclick = login;
+}
+
+function login()
+{
+	
+var userEmail = document.getElementById("userEmail").value;
+<a href="Home.html"></a>
+
+}
