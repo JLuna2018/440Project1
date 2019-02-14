58ac656eb3b0368e3cae4f50606d57d8b42a828f
jluna12 <jorge.luna.1@asu.edu>
2019-02-14 12:10:16 -0700

    added time dropdown and submit button for parking garages & sign up button on login page

--- link.js
+++ link.js
@@ -2,13 +2,20 @@
 window.onload = function()
 {
     //window.alert("The page just finished loading.");
-    document.getElementById("button").onclick = login;
+    document.getElementById("loginButton").onclick = login;
+    document.getElementById("submitButton").onclick = submit;
+    document.getElementById("fultonParking").innerHTML = 500;
 }
 
 function login()
 {
 	
 var userEmail = document.getElementById("userEmail").value;
-<a href="Home.html"></a>
+
+
+}
+
+function submit()
+{
 
 }
